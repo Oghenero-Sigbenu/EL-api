e16cb940b834bea8baac35bdf8839c3cfc45c368
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2020-10-01 13:28:33 +0100

    code fix

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("heroku_c853b2fbc06053d","bbecd1000a6b4f", "5f9e1fb4", {
-    host: "us-cdbr-east-02.cleardb.com",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: "mysql",
     logging: true,
-    port: 3306
+    port: process.env.DB_PORT
 })
 
 module.exports = sequelize;
